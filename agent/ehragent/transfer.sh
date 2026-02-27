b0f32b6eba1e1ceaf361dbc6a5cbdf19096345bc
Mikel Rodriguez <mikelrodriguez@Mikels-iMac.local>
2026-02-26 22:01:54 -0500

    fix deltas

---
+++ agent/ehragent/transfer.sh
@@ -0,0 +1,10 @@
+python ./ehragent/main.py -m bge -t
+python ./ehragent/main.py -m bge
+python ./ehragent/main.py -m dpr -t
+python ./ehragent/main.py -m dpr
+python ./ehragent/main.py -m ance -t
+python ./ehragent/main.py -m ance
+python ./ehragent/main.py -m realm -t
+python ./ehragent/main.py -m realm
+python ./ehragent/main.py -m ada -t
+python ./ehragent/main.py -m ada
---
+++ agentdriver/motion_planner/trl/examples/README.md
@@ -0,0 +1,3 @@
+# Examples
+
+Please check out https://Anonymous.co/docs/trl/example_overview for documentation on our examples.
